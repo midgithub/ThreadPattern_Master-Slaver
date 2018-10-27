a273a6c97ef1b96308362f7c43ae1a73b7504fab
JiepengTan <jiepengtan@gmail.com>
2018-10-27 23:46:37 +0800

    Init
    [*]Init commit

---
+++ Assets/ThreadPattern/Init.cs
@@ -0,0 +1,22 @@
+﻿//=====================================================
+// - FileName:      Init.cs
+// - Created:       #AuthorName#
+// - UserName:      #CreateTime#
+// - Email:         #AuthorEmail#
+//======================================================
+
+using UnityEngine;
+using System.Collections;
+namespace TurnBase{
+    public class Init : MonoBehaviour {
+        void OnInit(){
+
+        }
+        void OnUpdate (long _elpse_ms) {
+
+        }
+        
+        void Destroy() {
+        }
+    }
+}
